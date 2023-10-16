e0c3f5267cd5fc03b445a816a9b3a6cd123c3f7c
AlexS1r <gilagalex@gmail.com>
2023-10-16 15:54:39 +0300

    Initial commit

--- Create.r
+++ Create.r
@@ -2,4 +2,13 @@ library("devtools")
 
 create_project("F:/Bioinformatics/VS_Code/Alice's_Adventures_in_Wonderland_Project")
 
+
 use_git()
+
+use_r("All_in_one")
+
+check()
+
+use_mit_license()
+
+use_github()
