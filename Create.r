21afa88532440f941c356c3158e465b9feef4e42
AlexS1r <gilagalex@gmail.com>
2023-10-16 16:21:27 +0300

    Update the README file

--- Create.r
+++ Create.r
@@ -2,7 +2,8 @@ library("devtools")
 
 create_project("F:/Bioinformatics/VS_Code/Alice's_Adventures_in_Wonderland_Project")
 
-awdawd
+proj_get()
+proj_set()
 
 use_git()
 
@@ -15,3 +16,11 @@ use_mit_license()
 use_github()
 
 use_readme_rmd()
+
+build_readme()
+
+devtools::document(setwd("F:/Bioinformatics/VS_Code/Alice's_Adventures_in_Wonderland_Project"))
+
+knitr::knit("F:/Bioinformatics/VS_Code/Alice's_Adventures_in_Wonderland_Project/README.Rmd")
+
+file.remove(".git/hooks/pre-commit")
